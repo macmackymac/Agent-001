8da25f9e7530a7640796dabf3e876563f21ce489
macmackymac <307942590+macmackymac@users.noreply.github.com>
2026-07-31 08:32:25 +0800

    Implement PersonaManager for loading personas
    Added PersonaManager class to manage personas from markdown files.

--- core/personas.py
+++ core/personas.py
@@ -1,1 +1,22 @@
+import os
 
+
+class PersonaManager:
+
+    def __init__(self, folder="personas"):
+        self.folder = folder
+
+    def load(self):
+        personas = {}
+
+        if not os.path.isdir(self.folder):
+            return personas
+
+        for file in os.listdir(self.folder):
+            if file.endswith(".md"):
+                path = os.path.join(self.folder, file)
+
+                with open(path, "r", encoding="utf-8") as f:
+                    personas[file[:-3]] = f.read()
+
+        return personas
