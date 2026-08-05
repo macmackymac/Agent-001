3abc3c7b410e25da6b3cf27d51b3fb6d4e2c24eb
Randell Santos <307942590+macmackymac@users.noreply.github.com>
2026-08-06 00:25:20 +0800

    commit

--- appa-backend/app/main.py
+++ appa-backend/app/main.py
@@ -1,9 +1,20 @@
 from fastapi import FastAPI
+from fastapi.middleware.cors import CORSMiddleware
 from pydantic import BaseModel
 
 app = FastAPI(
     title="APPA Backend",
     version="0.1.0"
+)
+
+app.add_middleware(
+    CORSMiddleware,
+    allow_origins=[
+        "http://localhost:5173",
+    ],
+    allow_credentials=True,
+    allow_methods=["*"],
+    allow_headers=["*"],
 )
 
 
